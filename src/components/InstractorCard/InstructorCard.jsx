80a53954026ab27e65b7f59681a91fa37bbfb6c3
magdy Abo El-nour <mm246344@gmail.com>
2024-10-27 22:51:20 +0300

    edits

--- src/components/InstractorCard/InstructorCard.jsx
+++ src/components/InstractorCard/InstructorCard.jsx
@@ -21,34 +21,36 @@ const CardComponent = ({
   const { t } = useTranslation();
   return (
     <div className='p-6'>
-      <div className="p-4 max-w-md mx-auto bg-white rounded-xl shadow-lg">
+      <div className="p-4 max-w-md mx-auto bg-gradient-to-r from-white to-gray-300 rounded-xl shadow-lg transition-transform transform hover:scale-105 duration-300">
         {/* Avatar Section */}
-        <div className="mx-auto w-28 h-28 overflow-hidden rounded-3xl">
+        <div className="mx-auto w-28 h-28 overflow-hidden rounded-full shadow-lg border-4 border-gray-500">
           <img className="w-full h-full object-cover" src={img} alt="Avatar" />
         </div>
 
         {/* Name and Rating Section */}
         <div className="text-center mt-4">
-          <h2 className="text-xl font-bold text-gray-800">
-            {firstName} {lastName}
+          <h2 className="text-2xl font-bold text-gray-800 hover:text-blue-600 transition-colors duration-300">
+            Hi, I'm {firstName} {lastName}
           </h2>
           <div className="flex justify-center items-center mt-2">
             <div className="flex items-center text-yellow-400 text-lg mb-4">
               <AiFillStar className="mr-1" />
-              <span className="font-semibold text-yellow-500">( {Review} {t("Reviews")})</span>
+              <span className="font-semibold text-yellow-500">
+                ({Review} {t("Reviews")})
+              </span>
             </div>
           </div>
         </div>
 
         {/* Information Section */}
-        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-2">
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-2">
           <div className='text-start'>
-            <span className="block text-gray-500">{t('teacher')}: {country}</span>
-            <span className="block text-gray-500">{t('From')}: {country}</span>
+            <span className="block text-gray-500">{t('teacher')}: <span className="font-semibold text-gray-800">{country}</span></span>
+            <span className="block text-gray-500">{t('From')}: <span className="font-semibold text-gray-800">{country}</span></span>
           </div>
           <div className='text-start md:text-center'>
-            <span className="block text-gray-500">{t('Lessons')}: {Lessons}</span>
-            <span className="block text-gray-500">{t('Students')}: {students}</span>
+            <span className="block text-gray-500">{t('Lessons')}: <span className="font-semibold text-gray-800">{Lessons}</span></span>
+            <span className="block text-gray-500">{t('Students')}: <span className="font-semibold text-gray-800">{students}</span></span>
           </div>
         </div>
 
@@ -58,7 +60,7 @@ const CardComponent = ({
             {about ? about : "Lorem ipsum dolor sit, amet consectetur adipisicing elit."}
           </p>
           <button
-            className="text-blue-600 pt-4 focus:outline-none hover:underline"
+            className="text-blue-600 pt-4 focus:outline-none hover:underline transition-colors duration-300"
             onClick={toggleReadMore}
           >
             {isExpanded ? t('Read Less') : t('Read More')}
@@ -66,6 +68,7 @@ const CardComponent = ({
         </div>
       </div>
     </div>
+
   );
 };
 
--- src/components/UpIcon.jsx
+++ src/components/UpIcon.jsx
@@ -32,11 +32,18 @@ export default function UpIcon() {
 
     return (
         <div
-            className={`fixed bottom-5 left-[1.3rem] bg-gradient-to-r from-orange-500 to-purple-500 hover:from-purple-500 hover:to-orange-500 text-white py-2 px-3 rounded-full cursor-pointer shadow-lg transition-all -translate-x-10 translate-y-10 z-50 duration-500 ${isVisible ? 'opacity-100 scale-100 translate-x-0 translate-y-0' : 'opacity-0 scale-0 -translate-x-10 translate-y-10'
-                }`}
+            className={`fixed bottom-5 left-[1.3rem] 
+            bg-gradient-to-r from-orange-500 to-purple-500 
+            hover:from-purple-500 hover:to-orange-500 
+            text-white rounded-full 
+            cursor-pointer shadow-lg transition-all 
+            duration-500 
+            ${isVisible ? 'opacity-100 scale-100 translate-0' : 'opacity-0 scale-0 -translate-x-10 translate-y-10'}
+            z-50
+            w-10 h-10 flex items-center justify-center`}
             onClick={scrollToTop}
         >
-            <FontAwesomeIcon icon={faAngleUp} />
+            <FontAwesomeIcon icon={faAngleUp} className="text-xl" />
         </div>
     );
 }
